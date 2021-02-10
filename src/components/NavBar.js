98e97218b45f48244f82c0212c2afee861a1a1e2
Carlos Del Valle <cadelvalledieguez@gmail.com>
2021-02-11 00:10:32 +1100

    Add NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,26 @@
 import React from "react"
+import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
-    return <h1>NavBar Page!</h1>
+    return (
+        <header>
+            <div>
+                <nav>
+                    <NavLink to="/" exact>
+                        Carlos Del Valle
+                    </NavLink>
+                    <NavLink to="/post/:slug">
+                        Blog Posts
+                    </NavLink>
+                    <NavLink to="/project">
+                        Projects
+                    </NavLink>
+                    <NavLink to="/about">
+                        About Me!
+                    </NavLink>
+
+                </nav>
+            </div>
+        </header>
+    )
 }
