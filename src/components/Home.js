e583c7fea6d60cc1fef0a176daf5a17417eaf9f8
Carlos Del Valle <cadelvalledieguez@gmail.com>
2021-02-11 01:26:05 +1100

    Add Component Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,21 @@
 import React from "react"
+import image from "../background.png"
 
 export default function Home() {
-    return <h1>Carlos Del Valle</h1>
+    return (
+        <main>
+            <img
+                src={image}
+                alt="Random shitty background"
+                className="absolute object-cover w-full h-full"
+            />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1
+                    className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+                    Aloha. I'm Carlos.
+                </h1>
+            </section>
+        </main>
+    )
 }
 
